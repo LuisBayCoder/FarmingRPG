5a70f116a4d5d51c3c8f9c4155026a30f51c883b
Luis Mendoza <luismunity@gmail.com>
2024-06-21 14:31:04 -0700

    implement status bar

---
+++ Assets/Scripts/GUI/StatusBar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Image bar;
+
+}
