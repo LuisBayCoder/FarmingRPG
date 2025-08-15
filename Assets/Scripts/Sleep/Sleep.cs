94c9e02648977473d2ed128a1c10803fb560558c
Luis Mendoza <luismunity@gmail.com>
2025-08-15 13:19:39 -0700

    Add Sleep functionality with collider detection and logging

---
+++ Assets/Scripts/Sleep/Sleep.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleep : MonoBehaviour
+{
+    //if the player enters the collider trigger a sleep method. 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SleepPlayer();
+        }
+    }
+
+    private void SleepPlayer()
+    {
+        Debug.Log("Player is sleeping...");
+    }
+}
